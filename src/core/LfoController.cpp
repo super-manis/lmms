07078ca196ee165c50d4e753dc44e78ffdd0a282
super-manis <74963516+super-manis@users.noreply.github.com>
2024-06-11 23:06:29 +0300

    Update LfoController.cpp

--- src/core/LfoController.cpp
+++ src/core/LfoController.cpp
@@ -40,7 +40,7 @@ namespace lmms
 LfoController::LfoController( Model * _parent ) :
 	Controller( ControllerType::Lfo, _parent, tr( "LFO Controller" ) ),
 	m_baseModel( 0.5, 0.0, 1.0, 0.001, this, tr( "Base value" ) ),
-	m_speedModel( 2.0, 0.01, 20.0, 0.0001, 20000.0, this, tr( "Oscillator speed" ) ),
+	m_speedModel( 2.0, 0.01, 20.0, 0.00000001, 20000.0, this, tr( "Oscillator speed" ) ),
 	m_amountModel( 1.0, -1.0, 1.0, 0.005, this, tr( "Oscillator amount" ) ),
 	m_phaseModel( 0.0, 0.0, 360.0, 4.0, this, tr( "Oscillator phase" ) ),
 	m_waveModel( static_cast<int>(Oscillator::WaveShape::Sine), 0, Oscillator::NumWaveShapes,
